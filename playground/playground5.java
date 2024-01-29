8a7eb209ef8dc8d5f47c19aa70d0ab9c58832eb0
PavlinK1 <PepstoFTW@gmail.com>
2024-01-29 22:36:56 +0200

    exercise: Add Homework from sixth lecture
    Signed-off-by: PavlinK1 <PepstoFTW@gmail.com>

---
+++ playground/playground5.java
@@ -0,0 +1,22 @@
+package playground;
+
+import java.util.Scanner;
+
+public class playground5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number");
+        double num2 = input.nextDouble();
+        System.out.printf("Num is %.5f%n", num2);
+
+        byte Ivancho = 2;
+        byte Ani = 5;
+        byte Mariika = 6;
+        byte Pencho = 4;
+        byte Borislavcho = 5;
+
+        System.out.printf("| Ivancho | %1d |%n| Ani | %1d |%n| Mariika | %1d |%n| Pencho  | %1d |%n| Borislvacho | %1d |",
+                Ivancho, Ani, Mariika, Pencho, Borislavcho);
+
+    }
+}
